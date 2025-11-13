9f230cff5466368761c865493eed52aa4f63da79
Owen Moogk <owenmoogk@gmail.com>
2025-11-13 11:15:58 -0500

    Almost working for 3a

--- lab_2/script_3_1a.m
+++ lab_2/script_3_1a.m
@@ -2,61 +2,81 @@
 data1 = load('2_1a.mat');
 data2 = load('2_1b.mat');
 
-% Define plant parameters
-m = 2.45e-4;      % mass [kg]
-b = 0.010339;     % damping coefficient [N*s/m]
+% Load simulated data from Simulink (contains 'out' object)
+sim1_data = load('3_1a_2_1a.mat');  % contains 'out'
+sim2_data = load('3_1a_2_1b.mat');  % contains 'out'
 
-% Define PD controller gains
-Kp = 3947.86 * m;     % proportional gain
-Kd = 0.008134;    % derivative gain
+% --- Extract signals from the out object and convert to numeric arrays ---
+% For sim1
+sim1_time  = double(sim1_data.out.get('tout'));  % simulation time
+sim1_x_out = double(sim1_data.out.get('x_sim').Data);  % simulated position
+sim1_u     = double(sim1_data.out.get('u_sim').Data);  % control signal
 
-% Create Laplace variable
-s = tf('s');
+% For sim2
+sim2_time  = double(sim2_data.out.get('tout'));
+sim2_x_out = double(sim2_data.out.get('x_sim').Data);
+sim2_u     = double(sim2_data.out.get('u_sim').Data);
 
-% Closed-loop PD-controlled plant
-plant = 1/(s*(m*s + b));
-controller = Kp + Kd*s;
-sys = feedback(controller*plant, 1);  % unity negative feedback
+% --- Interpolate simulated outputs to experimental time ---
+sim1_x_interp = interp1(sim1_time, sim1_x_out, data1.time, 'linear');
+sim2_x_interp = interp1(sim2_time, sim2_x_out, data2.time, 'linear');
 
-% Original time vectors
-t1 = data1.time;
-t2 = data2.time;
-
-% Reference signals
-x_r1 = data1.x_r;
-x_r2 = data2.x_r;
-
-% --- Resample input to avoid undersampling ---
-dt = 1e-4;  % smaller than 2.4e-4 as suggested by warning
-
-t1_fine = t1(1):dt:t1(end);
-x_r1_fine = interp1(t1, x_r1, t1_fine, 'linear');
-
-t2_fine = t2(1):dt:t2(end);
-x_r2_fine = interp1(t2, x_r2, t2_fine, 'linear');
-
-% Simulate responses
-y_sim1 = lsim(sys, x_r1_fine, t1_fine);
-y_sim2 = lsim(sys, x_r2_fine, t2_fine);
+% --- Compute tracking errors ---
+err1 = data1.x_r - sim1_x_interp;
+err2 = data2.x_r - sim2_x_interp;
 
 % --- Plot 2_1a ---
 figure;
-plot(t1, x_r1, 'Color', [0.6 0.6 0.6], 'LineWidth', 1); hold on;
-plot(t1, data1.x_out, 'b', 'LineWidth', 1.2);
-plot(t1_fine, y_sim1, 'r--', 'LineWidth', 1.2);
-title('2\_1a Response: Experimental vs Simulated');
-xlabel('Time (s)');
+
+% 1: System response
+subplot(3,1,1);
+plot(data1.time, data1.x_r, 'Color', [0.6 0.6 0.6], 'LineWidth', 1); hold on;
+plot(data1.time, data1.x_out, 'b', 'LineWidth', 1.2);
+plot(data1.time, sim1_x_interp, 'r--', 'LineWidth', 1.2);
+title('2\_1a: Position Response');
 ylabel('Position (mm)');
 legend('Reference', 'Experimental', 'Simulated');
 grid on;
 
+% 2: Tracking error
+subplot(3,1,2);
+plot(data1.time, err1, 'k', 'LineWidth', 1.2);
+title('Tracking Error (x_r - x_{out})');
+ylabel('Error (mm)');
+grid on;
+
+% 3: Control signal
+subplot(3,1,3);
+plot(sim1_time, sim1_u, 'm', 'LineWidth', 1.2);
+title('Control Signal u(t)');
+xlabel('Time (s)');
+ylabel('u(t)');
+grid on;
+
 % --- Plot 2_1b ---
 figure;
-plot(t2, x_r2, 'Color', [0.6 0.6 0.6], 'LineWidth', 1); hold on;
-plot(t2, data2.x_out, 'b', 'LineWidth', 1.2);
-plot(t2_fine, y_sim2, 'r--', 'LineWidth', 1.2);
-title('2\_1b Response: Experimental vs Simulated');
-xlabel('Time (s)');
+
+% 1: System response
+subplot(3,1,1);
+plot(data2.time, data2.x_r, 'Color', [0.6 0.6 0.6], 'LineWidth', 1); hold on;
+plot(data2.time, data2.x_out, 'b', 'LineWidth', 1.2);
+plot(data2.time, sim2_x_interp, 'r--', 'LineWidth', 1.2);
+title('2\_1b: Position Response');
 ylabel('Position (mm)');
 legend('Reference', 'Experimental', 'Simulated');
+grid on;
+
+% 2: Tracking error
+subplot(3,1,2);
+plot(data2.time, err2, 'k', 'LineWidth', 1.2);
+title('Tracking Error (x_r - x_{out})');
+ylabel('Error (mm)');
+grid on;
+
+% 3: Control signal
+subplot(3,1,3);
+plot(sim2_time, sim2_u, 'm', 'LineWidth', 1.2);
+title('Control Signal u(t)');
+xlabel('Time (s)');
+ylabel('u(t)');
 grid on;
